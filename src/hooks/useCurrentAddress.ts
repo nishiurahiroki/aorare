575a474ac6ccf86ce3a7edd2aff628c095e1ba34
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-01-21 02:03:30 +0000

    Create hooks directory.

---
+++ src/hooks/useCurrentAddress.ts
@@ -0,0 +1,33 @@
+import useSWR, {SWRResponse} from 'swr'
+
+const geoLocateFetcher = () => {
+  return new Promise((resolve, reject) => {
+    async function onSuccess({ coords }) {
+      const response = await fetch(`https://aginfo.cgk.affrc.go.jp/ws/rgeocode.php?json&lat=${coords.latitude}&lon=${coords.longitude}`)
+      const json = await response.json()
+      resolve(json)
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, reject);
+  })
+}
+
+const useCurrentAddress = () => {
+  const { data, error } : SWRResponse = useSWR('geolocation', geoLocateFetcher)
+
+  if(!data) return { isLoading : true }
+  if(error) return { error }
+
+  const [ local ] = data?.result?.local
+  const { mname } = data?.result?.municipality
+  const { pname } = data?.result?.prefecture
+
+  return {
+    pname,
+    mname,
+    section : local.section,
+    homenumber : local.homenumber,
+    isLoading : false,
+    error : null
+  }
+}
+export default useCurrentAddress
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,20 @@
 import Head from 'next/head'
 
-import useSWR, {SWRResponse} from 'swr'
+import useCurrentAddress from '../hooks/useCurrentAddress'
 
-
-
-const geoLocateFetcher = () => {
-  return new Promise((resolve, reject) => {
-    async function onSuccess({ coords }) {
-      const response = await fetch(`https://aginfo.cgk.affrc.go.jp/ws/rgeocode.php?json&lat=${coords.latitude}&lon=${coords.longitude}`)
-      const json = await response.json()
-      resolve(json)
-    }
-    navigator.geolocation.getCurrentPosition(onSuccess, reject);
-  })
-}
 
 export default function Index() {
-  const { data, error } : SWRResponse = useSWR('geolocation', geoLocateFetcher)
+  const {
+    pname,
+    mname,
+    section,
+    homenumber,
+    isLoading : isLoadingAddress,
+    error : errorLoadingAddress
+  } = useCurrentAddress()
 
-  if(!data) return 'Loading!'
-  if(error) return 'Error'
-
-  const [ local ] = data?.result?.local
-  const { mname } = data?.result?.municipality
-  const { pname } = data?.result?.prefecture
+  if(isLoadingAddress) return 'Loading address'
+  if(errorLoadingAddress) return 'Error loading address'
 
   return (
     <div>
@@ -38,8 +29,8 @@ export default function Index() {
         <div className='text-4xl text-red-500 text-center font-semibold'>
           <span>{pname}</span>
           <span>{mname}</span>
-          <span>{local.section}</span>
-          <span>{local.homenumber}</span>
+          <span>{section}</span>
+          <span>{homenumber}</span>
         </div>
         <div className="text-2xl text-black-500 text-center font-semibold">です</div>
 
