39782dacc81960faaafc621ac92910de5612b2af
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-01-22 04:39:51 +0000

    PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
+import Head from 'next/head'
+
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>煽られた時に見るページ</title>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
